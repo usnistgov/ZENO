063c65ebca84911546575e165dfc3f3802a13f32
Derek Juba <derek.juba@nist.gov>
2018-12-18 14:09:16 -0500

    Updated version number to 5.1.2

--- cpp/src/cmdline.h
+++ cpp/src/cmdline.h
@@ -31,7 +31,7 @@ extern "C" {
 
 #ifndef CMDLINE_PARSER_VERSION
 /** @brief the program version */
-#define CMDLINE_PARSER_VERSION "5.1"
+#define CMDLINE_PARSER_VERSION "5.1.2"
 #endif
 
 /** @brief Where the command line options are stored */
